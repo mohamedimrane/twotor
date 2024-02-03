c3c82a509d9277a9f7ec51d36263c3f14740bbf9
Mohamed Imrane Chehabi <38333092+mohamedimrane@users.noreply.github.com>
2024-02-03 21:37:26 +0100

    Remove table schema useless duplication

--- data/schema/twoot.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE twoots (
-    ID INTEGER PRIMARY KEY,
-    contents text NOT NULL,
-    created_at datetime NOT NULL,
-    user_ID INTEGER NOT NULL,
-    FOREIGN KEY (user_ID) REFERENCES users(ID)
-);
